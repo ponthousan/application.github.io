b8d0c56897f01985e42d37b107f67d63dd83eac4
ponthousan <ponnayiram89@gmail.com>
2021-03-14 08:55:16 -0700

    init commit changes

---
+++ src/app/app-header/app-header/app-header.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppHeaderRoutingModule } from './app-header-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppHeaderRoutingModule
+  ]
+})
+export class AppHeaderModule { }
